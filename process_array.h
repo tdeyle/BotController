e3621c6cb2409ba4ea088871a4a8889d30af259a
tdeyle <theo.deyle@gmail.com>
2013-12-16 22:00:24 -0500

    Nighttime Work
    Finished mocking up cython version of 'updateFromLPS'. So far works
    well. Too tired, need to sleep to do more code....

--- process_array.h
+++ process_array.h
@@ -1,5 +1,5 @@
 void initialize(double *GPS_arr);
 void process(double *GPS_arr, double *LPS_arr, int *dist_arr, double origx, double origy, double theta);
 void setArray(double *arr, int rows, int cols, double value);
-void detectHits(double *LPS_arr, int *dist_arr, double theta, double origx, double origy);
-void assignOccupancy(double *LPS_arr, int offx, int offy, double hitx, double hity, double origx, double origy, int arc, int senseObstacle);
+void detectHits(double *LPS_arr, int *dist_arr, double theta);
+void assignOccupancy(double *LPS_arr, int offx, int offy, double hitx, double hity, int arc, int senseObstacle);
